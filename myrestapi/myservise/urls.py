823ea383af15d82404dcfff1c6157497c45a4a3f
m04kA <60301384+m04kA@users.noreply.github.com>
2022-08-23 14:13:38 +0300

    Routers V2

--- myrestapi/myservise/urls.py
+++ myrestapi/myservise/urls.py
@@ -1,24 +1,14 @@
 from django.urls import path, include
-
-from . import views
 from .views import BooksViewSet, AuthorsViewSet, GenresViewSet
 from rest_framework import routers
 
-routers_book = routers.DefaultRouter()
-routers_book.register(r'books', BooksViewSet)
+routers = routers.DefaultRouter()
+routers.register(r'books', BooksViewSet)
 
-routers_author = routers.DefaultRouter()
-routers_author.register(r'authors', AuthorsViewSet)
+routers.register(r'authors', AuthorsViewSet)
 
-routers_genre = routers.DefaultRouter()
-routers_genre.register(r'genres', GenresViewSet)
-
-
-
+routers.register(r'genres', GenresViewSet)
 
 urlpatterns = [
-    path('hi/', views.HelloWorld.as_view()),
-    path('', include(routers_book.urls)),
-    path('', include(routers_author.urls)),
-    path('', include(routers_genre.urls)),
+    path('', include(routers.urls)),
 ]
--- myrestapi/myservise/views.py
+++ myrestapi/myservise/views.py
@@ -1,13 +1,6 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import viewsets, filters
 from .serializers import BooksSerializer, AuthorsSerializer, GenresSerializer
 from .models import Books, Authors, Genres
-
-
-class HelloWorld(APIView):
-    def get(self, request):
-        return Response({'message': 'Hello World!'}, status=200)
 
 
 class BooksViewSet(viewsets.ModelViewSet):
